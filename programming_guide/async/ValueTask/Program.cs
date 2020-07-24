b19b19ed2ad45fe03578e6c18962864a84c88272
Victor Joseph <victornjoseph@gmail.com>
2020-07-24 18:42:02 +0300

    add value task implementation

---
+++ programming_guide/async/ValueTask/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+
+
+namespace ValueTask
+{
+    class Program
+    {
+        private static Random? rnd;
+
+        static async Task Main(string[] args)
+        {
+
+            Console.WriteLine($"You rolled {await GetRollsAsync()}");
+        }
+
+        static private async ValueTask<int> GetRollsAsync()
+        {
+            Console.WriteLine("...Shaking the dice...");
+            int roll1 = await RollAsync();
+            int roll2 = await RollAsync();
+            return roll1 + roll2;
+        }
+
+        static private async ValueTask<int> RollAsync()
+        {
+            if (rnd == null)
+            {
+                rnd = new Random();
+            }
+
+            await Task.Delay(500);
+            int diceRoll = rnd.Next(1, 7);
+            return diceRoll;
+        }
+    }
+}
