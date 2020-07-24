a398eb625c9b5a2485fad58275f9fc30e36082bf
Victor Joseph <victornjoseph@gmail.com>
2020-07-24 22:03:26 +0300

    async streams

---
+++ programming_guide/async/AsyncStream/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+
+namespace AsyncStream
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            await foreach (var word in ReadWordsFromStream()){
+                Console.WriteLine(word);
+            }
+        }
+
+        public static async IAsyncEnumerable<string> ReadWordsFromStream()
+        {
+            string data =
+                @"This is a line of text.
+                Here is the second line of text.
+                And there is one more for good measure.
+                Wait, there is the penultimate line.";
+            using var readStream = new StringReader(data);
+
+            string? line = await readStream.ReadLineAsync();
+            while (line != null)
+            {
+                var words = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                foreach (var word in words)
+                {
+                    yield return word;
+                }
+                
+                line = await readStream.ReadLineAsync();
+            }
+        }
+    }
+}
