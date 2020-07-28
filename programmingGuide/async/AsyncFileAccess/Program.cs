4e153615a4cd6255962372f233262503576e7bc0
Victor Joseph <victornjoseph@gmail.com>
2020-07-28 16:14:49 +0300

    Async file access

---
+++ programmingGuide/async/AsyncFileAccess/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+
+namespace AsyncFileAccess
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string filePath = @"temp.txt";
+            string fileText = "Hello, World!\r\n";
+
+            await WriteTextAsync(filePath, fileText);
+            Console.WriteLine("Success!");
+        }
+
+        private static async Task WriteTextAsync(string filePath, string text)
+        {
+            byte[] encodedText = Encoding.Unicode.GetBytes(text);
+
+            using (FileStream writeStream = new FileStream(filePath,
+                FileMode.Append, FileAccess.Write, FileShare.None,
+                bufferSize: 4096, useAsync: true))
+            {
+                await writeStream.WriteAsync(encodedText, 0, encodedText.Length);
+            };
+        }
+    }
+}
